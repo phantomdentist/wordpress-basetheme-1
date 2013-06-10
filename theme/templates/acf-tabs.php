eb2c10d212af8bdd5c2dccad858b2be2ce47e560
Tai havard <tai@tonguetiedmedia.com>
2013-06-10 23:50:11 +0100

    Added acf tabs templates

---
+++ theme/templates/acf-tabs.php
@@ -0,0 +1,45 @@
+<?php
+if( function_exists('get_field') ) {
+	if( get_field('tabs_repeater') ) { ?>
+		<script>
+		$(function() {
+		$( "#tabs" ).tabs();
+		});
+		</script>
+		
+		<div id="tabs">
+            <ul>
+            <?php $x = 1; ?>
+            <?php while( has_sub_field('tabs_repeater') ) { ?>
+                <li><a href="#tabs-<?php echo $x; ?>"><?php echo get_sub_field('tab_title'); ?></a></li>
+            <?php $x++; } ?>
+            </ul>
+            
+            <div id="tabs-1">
+            </div>
+            
+            <?php $x = 1; ?>
+            <?php while( has_sub_field('tabs_repeater') ) { ?>
+                <div id="tabs-<?php echo $x; ?>"><?php echo get_sub_field('tab_content'); ?></a></div>
+            <?php $x++; } ?>
+		</div>
+		<?php
+		}
+	}
+	?>
+
+<!--
+css
+	.ui-tabs
+	{
+		position: relative;
+	}
+	.ui-tabs .ui-tabs-panel 
+	{
+		display: block;
+	}
+	.ui-tabs .ui-tabs-hide
+	{
+		display: none!important;
+	}
+-->
